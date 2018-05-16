e354a5d145dc490c5ef73f3c77202d83045307c4
Warda Novitasari <vita.vee25@gmail.com>
2018-05-16 10:15:17 +0700

    Form Sales kalo buat daftarkan sales baru

---
+++ application/controllers/SalesBaru.php
@@ -0,0 +1,14 @@
+<?php
+class SalesBaru extends CI_Controller{
+	public function __construct(){
+		parent::__construct();
+		$this->load->library('session');
+	}
+
+	public function index(){
+		$this->session->set_userdata('username', 'admin');
+		$this->load->view('SalesBaru_view');
+	}
+
+}
+?>
