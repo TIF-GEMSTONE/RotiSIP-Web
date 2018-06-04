b19d4534efa8c33f92b0427abaecb562a39cf837
Warda Novitasari <vita.vee25@gmail.com>
2018-06-04 14:47:07 +0700

    no message

--- application/views/kasir.php
+++ application/views/kasir.php
@@ -1,4 +1,4 @@
-  <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -149,23 +149,23 @@
   <form action="http://localhost/RotiSIP-Web/Penjualan" method="post">
     <div class="form-group">
             <label class="control-label col-md-3" 
-              for="id_roti">Id Roti :</label>
+              for="id_barang">Id Roti :</label>
             <div class="col-md-5">
-              <input list="list_roti" class="form-control reset" 
+              <input list="list_barang" class="form-control reset" 
                 placeholder="Isi id..." name="id_roti" id="id_roti" 
-                autocomplete="off" onchange="showRoti(this.value)">
+                autocomplete="off" onchange="showBarang(this.value)">
                     <datalist id="list_roti">
-                     <?php foreach ($tabel_roti as $tabel_roti): ?>
-                        <option value="<?= $tabel_roti->id_roti ?>"><?= $tabel_roti->nama_roti ?></option>
-                      <?php endforeach ?>
+                     <?php foreach ($roti as $row){ ?>
+                  <option value="<?php echo $row->id_roti;?>"><?php echo $row->nama_roti;?></option>
+                  <?php }?>
                     </datalist>
             </div>
             
           </div>
-          <div id="nama_roti">
+          <div id="barang">
             <div class="form-group">
               <label class="control-label col-md-3" 
-                for="nama_roti">Nama Roti :</label>
+                for="nama_barang">Nama Roti :</label>
               <div class="col-md-8">
                 <input type="text" class="form-control reset" 
                   name="nama_roti" id="nama_roti" 
@@ -183,297 +183,17 @@
             </div>
             <div class="form-group">
               <label class="control-label col-md-3" 
-                for="jumlah_roti">Quantity :</label>
+                for="qty">Quantity :</label>
               <div class="col-md-4">
                 <input type="number" class="form-control reset" 
                   autocomplete="off" onchange="subTotal(this.value)" 
-                  onkeyup="subTotal(this.value)" id="jumlah_roti" min="0" 
-                  name="jumlah_roti" placeholder="Isi qty...">
+                  onkeyup="subTotal(this.value)" id="qty" min="0" 
+                  name="qty" placeholder="Isi qty...">
               </div>
             </div>
           </div>
-
-           <div class="form-group">
-            <label class="control-label col-md-3" 
-              for="sub_total">Sub-Total (Rp):</label>
-            <div class="col-md-8">
-              <input type="text" class="form-control reset" 
-                name="sub_total" id="sub_total" 
-                readonly="readonly">
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="col-md-offset-3 col-md-3">
-                <button type="button" class="btn btn-primary" 
-                id="tambah" onclick="addroti()">
-                  <i class="fa fa-cart-plus"></i> Tambah</button>
-            </div>
-          </div>
-            <!-- </div>
-          </div> --><!-- end panel-->
-          </div><!-- end col-md-8 -->
-          <div class="col-md-4 mb">
-        <div class="col-md-12">
-            <div class="form-group">
-              <label for="total" class="besar">Total (Rp) :</label>
-                <input type="text" class="form-control input-lg" 
-                name="total" id="total" placeholder="0"
-                readonly="readonly"  value="<?= number_format( 
-                      $this->cart->total(), 0 , '' , '.' ); ?>">
-            </div>
-            <div class="form-group">
-              <label for="bayar" class="besar">Bayar (Rp) :</label>
-                <input type="text" class="form-control input-lg uang" 
-                  name="bayar" placeholder="0" autocomplete="off"
-                  id="bayar" onkeyup="showKembali(this.value)">
-            </div>
-            <div class="form-group">
-              <label for="kembali" class="besar">Kembali (Rp) :</label>
-                <input type="text" class="form-control input-lg" 
-                name="kembali" id="kembali" placeholder="0"
-                readonly="readonly">
-            </div>
-        </div>
-          </div>
-    <!-- <input type='submit' name='btnSubmit' value="Tambah"><br> -->
+    <input type='submit' name='btnSubmit' value="Tambah"><br>
   </form>
-
-  <div class="col-md-4 mb">
-        <div class="col-md-12">
-            <div class="form-group">
-              <label for="total" class="besar">Total (Rp) :</label>
-                <input type="text" class="form-control input-lg" 
-                name="total" id="total" placeholder="0"
-                readonly="readonly"  value="<?= number_format( 
-                      $this->cart->total(), 0 , '' , '.' ); ?>">
-            </div>
-            <div class="form-group">
-              <label for="bayar" class="besar">Bayar (Rp) :</label>
-                <input type="text" class="form-control input-lg uang" 
-                  name="bayar" placeholder="0" autocomplete="off"
-                  id="bayar" onkeyup="showKembali(this.value)">
-            </div>
-            <div class="form-group">
-              <label for="kembali" class="besar">Kembali (Rp) :</label>
-                <input type="text" class="form-control input-lg" 
-                name="kembali" id="kembali" placeholder="0"
-                readonly="readonly">
-            </div>
-        </div>
-          </div><!-- end col-md-4 -->
-          </form>
-
-          <table id="table_transaksi" class="table table-striped 
-            table-bordered">
-        <thead>
-          <tr>
-              <th>No</th>
-              <th>Id Roti</th>
-              <th>Nama Roti</th>
-              <th>Harga</th>
-              <th>Quantity</th>
-              <th>Sub-Total</th>
-              <th>Aksi</th>
-          </tr>
-        </thead>
-        <tbody>
-        </tbody>
-      </table>
-      <div class="col-md-offset-8" style="margin-top:20px">
-        <button type="button" class="btn btn-primary btn-lg" 
-        id="selesai" disabled="disabled" 
-        onclick="alert('Belum ada action untuk save pejualan')">
-        Selesai <i class="fa fa-angle-double-right"></i></button>
-      </div>
-        </div>
-      </div>
-  </div>
-
-  <script type="text/javascript">
-
-  function showRoti(str) 
-  {
-
-      if (str == "") {
-          $('#nama_roti').val('');
-          $('#harga').val('');
-          $('#jumlah_roti').val('');
-          $('#sub_total').val('');
-          $('#reset').hide();
-          return;
-      } else { 
-        if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-             xmlhttp = new XMLHttpRequest();
-        } else {
-            // code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange = function() {
-             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                document.getElementById("tabel_roti").innerHTML = 
-                xmlhttp.responseText;
-            }
-        }
-        xmlhttp.open("GET", "<?= base_url(
-          'Penjualan/getroti') ?>/"+str,true);
-        xmlhttp.send();
-      }
-  }
-
-  function subTotal(jumlah_roti)
-  {
-
-    var harga = $('#harga').val().replace(".", "").replace(".", "");
-
-    $('#sub_total').val(convertToRupiah(harga*jumlah_roti));
-  }
-
-  function convertToRupiah(angka)
-  {
-
-      var rupiah = '';    
-      var angkarev = angka.toString().split('').reverse().join('');
-      
-      for(var i = 0; i < angkarev.length; i++) 
-        if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
-      
-      return rupiah.split('',rupiah.length-1).reverse().join('');
-  
-  }
-
-  var table;
-    $(document).ready(function() {
-
-      showKembali($('#bayar').val());
-
-      table = $('#table_transaksi').DataTable({ 
-        paging: false,
-        "info": false,
-        "searching": false,
-        "processing": true, //Feature control the processing indicator.
-        "serverSide": true, //Feature control DataTables' 
-        // server-side processing mode.
-        
-        // Load data for the table's content from an Ajax source
-        "ajax": {
-            "url": "<?= site_url('Penjualan/ajax_list_transaksi')?>",
-            "type": "POST"
-        },
-
-        //Set column definition initialisation properties.
-        "columnDefs": [
-        { 
-          "targets": [ 0,1,2,3,4,5,6 ], //last column
-          "orderable": false, //set not orderable
-        },
-        ],
-
-      });
-    });
-
-    function reload_table()
-    {
-
-      table.ajax.reload(null,false); //reload datatable ajax 
-    
-    }
-
-    function addroti()
-    {
-        var id_roti = $('#id_roti').val();
-        var jumlah_roti = $('#jumlah_roti').val();
-        if (id_roti == '') {
-          $('#id_roti').focus();
-        }else if(jumlah_roti == ''){
-          $('#jumlah_roti').focus();
-        }else{
-       // ajax adding data to database
-          $.ajax({
-            url : "<?= site_url('Penjualan/addroti')?>",
-            type: "POST",
-            data: $('#form_transaksi').serialize(),
-            dataType: "JSON",
-            success: function(data)
-            {
-               //reload ajax table
-               reload_table();
-            },
-            error: function (jqXHR, textStatus, errorThrown)
-            {
-                alert('Error adding data');
-            }
-        });
-
-          showTotal();
-          showKembali($('#bayar').val());
-          //mereset semua value setelah btn tambah ditekan
-          $('.reset').val('');
-        };
-    }
-
-    function deletebarang(id,sub_total)
-    {
-        // ajax delete data to database
-          $.ajax({
-            url : "<?= site_url('Penjualan/deleteroti')?>/"+id,
-            type: "POST",
-            dataType: "JSON",
-            success: function(data)
-            {
-               reload_table();
-            },
-            error: function (jqXHR, textStatus, errorThrown)
-            {
-                alert('Error deleting data');
-            }
-        });
-
-          var ttl = $('#total').val().replace(".", "");
-
-          $('#total').val(convertToRupiah(ttl-sub_total));
-
-          showKembali($('#bayar').val());
-    }
-
-    function showTotal()
-    {
-
-      var total = $('#total').val().replace(".", "").replace(".", "");
-
-      var sub_total = $('#sub_total').val().replace(".", "").replace(".", "");
-
-      $('#total').val(convertToRupiah((Number(total)+Number(sub_total))));
-
-    }
-
-    //maskMoney
-  $('.uang').maskMoney({
-    thousands:'.', 
-    decimal:',', 
-    precision:0
-  });
-
-  function showKembali(str)
-    {
-      var total = $('#total').val().replace(".", "").replace(".", "");
-      var bayar = str.replace(".", "").replace(".", "");
-      var kembali = bayar-total;
-
-      $('#kembali').val(convertToRupiah(kembali));
-
-      if (kembali >= 0) {
-        $('#selesai').removeAttr("disabled");
-      }else{
-        $('#selesai').attr("disabled","disabled");
-      };
-
-      if (total == '0') {
-        $('#selesai').attr("disabled","disabled");
-      };
-    }
-
-  </script>
   
     <a class="scroll-to-top rounded" href="#page-top">
       <i class="fa fa-angle-up"></i>
