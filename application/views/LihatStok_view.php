b326fe9b66a83ad5e965e2bd830572169aeda349
Warda Novitasari <vita.vee25@gmail.com>
2018-07-01 11:47:50 +0700

    navbar done

--- application/views/LihatStok_view.php
+++ application/views/LihatStok_view.php
@@ -26,67 +26,11 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-          <a class="nav-link" href="<?php echo base_url();?>Penjualan">
-            <i class="fa fa-fw fa-dashboard"></i>
-            <span class="nav-link-text">Penjualan</span>
-          </a>
-        </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Form Sales">
-          <a class="nav-link" href="<?php echo base_url();?>SalesBaru">
-            <i class="fa fa-fw fa-area-chart"></i>
-            <span class="nav-link-text">Sales</span>
-          </a>
-        </li>
-       <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Link">
-          <a class="nav-link" href="<?php echo base_url();?>Pesanan">
-            <i class="fa fa-fw fa-link"></i>
-            <span class="nav-link-text">Pesanan</span>
-          </a>
-        </li>
-    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Link">
-          <a class="nav-link" href="<?php echo base_url();?>Retur">
-            <i class="fa fa-fw fa-link"></i>
-            <span class="nav-link-text">Retur</span>
-          </a>
-        </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Example Pages">
-          <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseStok" data-parent="#exampleAccordion">
-            <i class="fa fa-fw fa-area-chart"></i>
-            <span class="nav-link-text">Stok</span>
-          </a>
-          <ul class="sidenav-second-level collapse" id="collapseStok">
-            <li>
-              <a href="<?php echo base_url();?>StokSales">Sales</a>
-            </li>
-            <li>
-              <a href="<?php echo base_url();?>StokSIP">SIP</a>
-            </li>
-          </ul>
-        </li>
-         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Menu Levels">
-          <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseLaporan" data-parent="#exampleAccordion">
-            <i class="fa fa-fw fa-sitemap"></i>
-            <span class="nav-link-text">Laporan</span>
-          </a>
-          <ul class="sidenav-second-level collapse" id="collapseLaporan">
-            <li>
-               <a href="<?php echo base_url();?>LaporanSales">Sales</a>
-            </li>
-            <li>
-              <a href="<?php echo base_url();?>LaporanSIP">SIP</a>
-            </li>
-          </ul>
-        </li>
-      </ul>
-      <ul class="navbar-nav sidenav-toggler">
-        <li class="nav-item">
-         <a class="nav-link text-center" id="sidenavToggler">
-            <i class="fa fa-fw fa-angle-left"></i>
-          </a>
-        </li>
-      </ul>
+
+      <?php
+        $this->load->view('Menu');
+      ?>
+      
       <ul class="navbar-nav ml-auto">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle mr-lg-2" id="alertsDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
--- application/views/UbahStok_view.php
+++ application/views/UbahStok_view.php
@@ -26,44 +26,11 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-          <a class="nav-link" href="<?php echo base_url();?>Penjualan">
-            <i class="fa fa-fw fa-dashboard"></i>
-            <span class="nav-link-text">Penjualan</span>
-          </a>
-        </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Form Sales">
-          <a class="nav-link" href="<?php echo base_url();?>SalesBaru">
-            <i class="fa fa-fw fa-area-chart"></i>
-            <span class="nav-link-text">Sales</span>
-          </a>
-        </li>
-       <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Link">
-          <a class="nav-link" href="<?php echo base_url();?>Pesanan">
-            <i class="fa fa-fw fa-link"></i>
-            <span class="nav-link-text">Pesanan</span>
-          </a>
-        </li>
-    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Link">
-          <a class="nav-link" href="<?php echo base_url();?>Retur">
-            <i class="fa fa-fw fa-link"></i>
-            <span class="nav-link-text">Retur</span>
-          </a>
-        </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Example Pages">
-          <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseStok" data-parent="#exampleAccordion">
-            <i class="fa fa-fw fa-area-chart"></i>
-            <span class="nav-link-text">Stok</span>
-          </a>
-          <ul class="sidenav-second-level collapse" id="collapseStok">
-            <li>
-              <a href="<?php echo base_url();?>StokSales">Sales</a>
-            </li>
-            <li>
-              <a href="<?php echo base_url();?>StokSIP">SIP</a>
-            </li>
-          </ul>
+      
+       <?php
+        $this->load->view('Menu');
+      ?>
+
         </li>
          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Menu Levels">
           <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseLaporan" data-parent="#exampleAccordion">
